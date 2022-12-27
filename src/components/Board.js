74e3a5e1bf5d53f8d0ffeac432875729815022b3
Pearu Pung <pungpearu@gmail.com>
2022-12-27 02:25:20 +0200

    #2 Make cell contents responsive to gameplay

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,43 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Board = () => {
 
     let playerOneSymbol = '❌'
     let playerTwoSymbol = '⭕'
+    let boardSize = 2
 
-    let gameTable = [
-        ['❌', '⭕', '⭕'],
-        ['❌', '⭕', '⭕'],
-        ['❌', '⭕', '⭕'],
-    ]
+    const [board, setBoardState] = useState([
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ])
+
+    const [player, setPlayerState] = useState(playerOneSymbol)
+
+    const selectCell = (row, col) => {
+        console.log(`Row: ${row}, Col: ${col}`)
+        if (row > boardSize || col > boardSize || board[row][col] !== '') {
+            return
+        }
+
+        const newBoard = board
+        newBoard[row][col] = player
+        setBoardState(newBoard)
+        // switch players
+        if (player === playerOneSymbol) {
+            setPlayerState(playerTwoSymbol)
+        }
+        else {
+            setPlayerState(playerOneSymbol)
+        }
+    }
 
     return (
         <div className="game-board">
-        {gameTable.map((row, rindex) => {
+        {board.map((row, rindex) => {
         return row.map((cell, cindex) => (
-            <div key={rindex + cindex} className="box">
+            <div onClick={() => {selectCell(rindex, cindex)}} key={rindex + cindex} className="box">
                 {cell}
             </div>
         ))
